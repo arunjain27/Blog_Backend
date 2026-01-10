2f32fb7a730ad0df89e8d10a6ce6f4a7f1ac5189
arunjain27 <arunjain2704@gmail.com>
2026-01-10 12:08:01 +0530

    Connection update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Routes = require('./Router/router.js');
 // ===== CRITICAL: CORS MUST COME FIRST =====
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000','https://musingsss.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'auth-token'],
     credentials: true,
